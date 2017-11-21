e448a6ee7dfdd619b11fef73cb27fe02ec6a0475
Manuel <m.a.c.p90@gmail.com>
2017-11-20 21:28:27 -0500

    works preatty much

--- src/app/session.service.ts
+++ src/app/session.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
-import { Observable } from 'rxjs/Rx';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class SessionService {
